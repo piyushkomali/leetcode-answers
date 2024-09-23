8b5263ec5093f718b0bc67f6681a8b68d8f7ef13
piyushkomali <159651542+piyushkomali@users.noreply.github.com>
2024-09-23 19:42:53 -0400

    explanation 121. best time to buy and sell stock

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,3 +1,11 @@
+#objective: use the array given to find the best time to buy and sell stock(cannot sell before you buy), then return max profit
+    # prices = [7,1,5,3,6,4]; best time to buy is i = 1 and sell is i = 4, maxprofit = 6-1 = 5
+
+# min needs to be highest possible value to make sure that the first array value is set to min
+# iterate through the prices array and compare min to current price to make sure you have the lowest price possible set to min and in turn the max profit
+# in the same loop calculate the profit from the min price to the current price you are looking at and decide whether that is greater than the max profit
+#                                    if it is greater than maxprofit than set that to maxprofit
+# return maxprofit
 class Solution:
     
     def maxProfit(self, prices: List[int]) -> int:
