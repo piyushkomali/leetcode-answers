454ea2ab6e005e6b2719c0632dd3e442774e0cfd
piyushkomali <159651542+piyushkomali@users.noreply.github.com>
2025-01-16 16:27:49 -0500

    Time: 30 ms (76.04%), Space: 19 MB (11.94%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.py
@@ -3,12 +3,10 @@ class Solution:
         length = len(arr)
         r = [0] * length
         m = arr[length-1]
-        for i in range(length-1,-1,-1):
-            if i==length-1:
-                r[i] = -1
-            else:
-                r[i]=m
-                m = max(m,arr[i])
+        for i in range(length-2,-1,-1):
+            r[i]=m
+            m = max(m,arr[i])
+        r[length-1]=-1
         return r
                 
 
