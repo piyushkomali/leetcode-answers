f388956015587f7947d934d2bdddb0c4385efdad
piyushkomali <komali.piyush@gmail.com>
2025-05-10 13:47:46 -0400

    hello

--- 0268-missing-number/0268-missing-number.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        sExpected = (n* (n+1))/2
-        sList = sum(nums)
-        return int(sExpected - sList)
-        
