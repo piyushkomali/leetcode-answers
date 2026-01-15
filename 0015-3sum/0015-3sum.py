676d94b7eab9234ab66a13368e0613d7bfe153e1
piyushkomali <159651542+piyushkomali@users.noreply.github.com>
2026-01-15 18:51:37 -0500

    Time: 512 ms (77.1%), Space: 20.7 MB (57.47%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,20 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        retList = set()
-        def twoSum2(nums,target):
-            l,r = 0, len(nums)-1
-            while (l < r):
-                if nums[l] + nums[r] < target: l+=1
-                elif nums[l] + nums[r] > target: r-=1
-                else: 
-                    retList.add(tuple([-1* target, nums[l],nums[r]]))
-                    l+=1
-                    r-=1
-            return []
-        nums = sorted(nums)
-        for i in range(len(nums)):
-            l = twoSum2(nums[i+1:], -1 * nums[i])
-        return list(retList)
+        retList = []
+        nums.sort()
 
+        for i, num in enumerate(nums):
+            # Skip duplicate for the first element of the triplet
+            if i > 0 and num == nums[i - 1]:
+                continue
 
-
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                current_sum = num + nums[l] + nums[r]
+                if current_sum > 0:
+                    r -= 1
+                elif current_sum < 0:
+                    l += 1
+                else:
+                    retList.append([num, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    # Skip duplicates for the second element of the triplet
+                    while l < r and nums[l] == nums[l - 1]:
+                        l += 1
+                    # Skip duplicates for the third element of the triplet
+                    while l < r and nums[r] == nums[r + 1]:
+                        r -= 1
+        return retList
